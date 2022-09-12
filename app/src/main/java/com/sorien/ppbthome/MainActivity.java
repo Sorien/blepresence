2b1daf5a68526731d6f6fa248158ccfd0ce07fac
Sorien <sorien@mail.com>
2022-09-12 15:24:32 +0200

    init

---
+++ app/src/main/java/com/sorien/ppbthome/MainActivity.java
@@ -0,0 +1,42 @@
+package com.sorien.ppbthome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Switch onOffSwitch = (Switch)findViewById(R.id.activate_switch_id);
+
+        onOffSwitch.setChecked(AdvertiserService.isServiceCreated());
+
+        onOffSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                                Manifest.permission.BLUETOOTH_ADVERTISE,
+                                Manifest.permission.BLUETOOTH
+                        }, 1);
+                    }
+                    startService(new Intent(MainActivity.this, AdvertiserService.class));
+                } else {
+                    stopService(new Intent(MainActivity.this, AdvertiserService.class));
+                }
+            }
+        });
+
+    }
+}
